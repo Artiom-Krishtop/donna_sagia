e99752d6685e0f51af3bf0957c473e0a5aa4a603
artiom <art_krishtop96@mail.ru>
2021-10-19 17:06:26 +0300

    4

--- local/templates/donna_saggia/components/bitrix/search.title/donna/template.php
+++ local/templates/donna_saggia/components/bitrix/search.title/donna/template.php
@@ -33,13 +33,13 @@ if($arParams["SHOW_INPUT"] !== "N"):?>
     </fieldlist>
 	</form>
 <?endif?>
-<!-- <script>
+<script>
 	BX.ready(function(){
 		new JCTitleSearch({
-			'AJAX_PAGE' : '<?//echo CUtil::JSEscape(POST_FORM_ACTION_URI)?>',
-			'CONTAINER_ID': '<?//echo $CONTAINER_ID?>',
-			'INPUT_ID': '<?//echo $INPUT_ID?>',
+			'AJAX_PAGE' : '<?echo CUtil::JSEscape(POST_FORM_ACTION_URI)?>',
+			'CONTAINER_ID': '<?echo $CONTAINER_ID?>',
+			'INPUT_ID': '<?echo $INPUT_ID?>',
 			'MIN_QUERY_LEN': 2
 		});
 	});
-</script> -->
+</script>
